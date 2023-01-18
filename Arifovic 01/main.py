0969ad2af2152eda419215fb9ea9518a530888ed
Ted <teddychenster@gmail.com>
2023-01-18 16:39:54 +0800

    Main function structure construction

--- Arifovic 01/main.py
+++ Arifovic 01/main.py
@@ -1,10 +1,21 @@
-from src.genetic import AGene
+from src.genetic import AGene, AGeneticAlgorithm
 import numpy as np
 
+from src.model import CurrencySubstitutionModel
+from mesa.time import RandomActivation
 
 def main():
+    ga = AGeneticAlgorithm(p_cross=0.6, p_mut=0.33)
 
-    print(np.array([False, False, False, True]).dot(1 << np.arange(4)))
+    abm_schedular = RandomActivation()
+    
+    model = CurrencySubstitutionModel(
+                genetic_algo_class  =   ga, 
+                n_agents            =   30,
+                G_1                 =   10,
+                G_2                 =   1)
+    
+    model.step()
 
 if __name__ == "__main__":
     main()
