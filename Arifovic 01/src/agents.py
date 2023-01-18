d4be09a0638a020ed78bb28eb5e69fa6ce587ddd
Ted <teddychenster@gmail.com>
2023-01-18 16:39:03 +0800

    Basis agent generating procedure

--- Arifovic 01/src/agents.py
+++ Arifovic 01/src/agents.py
@@ -4,6 +4,9 @@ from src.base.GA import EvaluableGene
 """
     EvaluableGene is to couple the agents with the genetic algorithm
 """
-class GA_Agent(mesa.agents, EvaluableGene):
-    pass
+class GA_Agent(mesa.Agent):
+    def __init__(self, unique_id:int, model:mesa.Model, evaluable_gene: EvaluableGene):
+        super().__init__(unique_id, model)
+
+        self.evaluable_gene: EvaluableGene = evaluable_gene
 
--- Arifovic 01/src/model.py
+++ Arifovic 01/src/model.py
@@ -1,20 +1,43 @@
 from typing import Callable
-import mesa 
-from dataclasses import dataclass
-from src.base.GA import BaseGeneticAlgorithm, BaseGene
+import mesa
+from src.agents import GA_Agent 
+from src.base.GA import BaseGeneticAlgorithm, BaseGene, EvaluableGene
+from src.genetic import AGene, AGeneticAlgorithm
 
-@dataclass
 class CurrencySubstitutionModel(mesa.Model):
     def __init__(self, 
                 genetic_algo_class : BaseGeneticAlgorithm,
+                scheduler: mesa.time.BaseScheduler,
                 n_agents: int   = 30,
                 G_1     : float = 10,
                 G_2     : float = 1,
                 ):
-        pass
+        self.ga         : AGeneticAlgorithm     = genetic_algo_class
+        self.n_agents   : int                   = n_agents
+        self.G_1        : float                 = G_1
+        self.G_2        : float                 = G_2
+        self.scheduler  : mesa.time.BaseScheduler = scheduler
 
-    def gene_evaluation_fn(self) ->  Callable[[BaseGene], float]:
+        self.generate_agents()
+        self.unique_id_generator = (i for i in range(10e5))
+
+
+    def gene_evaluation_fn(self) ->  Callable[..., float]:
         def ev_fn(gene:BaseGene) -> float:
             fitness = None
             return fitness
         return ev_fn
+    
+    def generate_agents(self):
+        for _ in self.n_agents:
+            new_id = next(self.unique_id_generator)
+            evaluable_gene = EvaluableGene(AGene())
+            
+            a = GA_Agent(
+                unique_id=new_id, 
+                model=self, 
+                evaluable_gene=evaluable_gene)
+            self.scheduler.add(a)
+
+    def step(self):
+        self.scheduler.step()
