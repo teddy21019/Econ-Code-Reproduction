bd95f0bac5ad16f508aed737469fcc808dc1756a
Ted <teddychenster@gmail.com>
2023-01-18 17:53:09 +0800

    Fix main

--- Arifovic 01/main.py
+++ Arifovic 01/main.py
@@ -2,20 +2,21 @@ from src.genetic import AGene, AGeneticAlgorithm
 import numpy as np
 
 from src.model import CurrencySubstitutionModel
-from mesa.time import RandomActivation
+from mesa.time import RandomActivationByType
 
 def main():
     ga = AGeneticAlgorithm(p_cross=0.6, p_mut=0.33)
 
-    abm_schedular = RandomActivation()
+    abm_schedular = RandomActivationByType
     
     model = CurrencySubstitutionModel(
                 genetic_algo_class  =   ga, 
                 n_agents            =   30,
                 G_1                 =   10,
-                G_2                 =   1)
+                G_2                 =   1,
+                scheduler=abm_schedular)
     
-    model.step()
+    model.random.shuffle
 
 if __name__ == "__main__":
     main()
